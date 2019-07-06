eebfbcd49fe645c89f42f8524f86108549485891
sonalbihani <sonalbihani9@gmail.com>
2019-07-07 01:24:44 +0530

    added maps

---
+++ android/app/src/main/java/lohith/com/careworks/EventMap.java
@@ -0,0 +1,32 @@
+package lohith.com.careworks;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class EventMap extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_event);
+        Button b;
+        b = findViewById(R.id.register);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+
+    }
+
+}
